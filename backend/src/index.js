5765e1f6f084d61c7cc1fa1cbe42ad2b0b464d10
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-06-24 08:36:14 +0500

    initial

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is running on port:" + PORT);
+  connectDB();
+});
+
+// https://cloud.mongodb.com/v2/6857b72c1154937f2c891764#/metrics/replicaSet/6857b7755b63986780a2451a/explorer
+// https://www.youtube.com/watch?v=ntKkVrQqBYY&ab_channel=Codesistency   22:03
