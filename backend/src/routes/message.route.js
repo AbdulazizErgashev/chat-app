82f275a2976c2d14478a2f956eca9df93c151232
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-07-05 13:53:10 +0500

    message controllers , route and models created

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
